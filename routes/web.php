33152a2de028bf9afdc44ae0ec2bc6b20f0391dd
KevinFaculdadeXD <engs-kevinrichardt@camporeal.edu.br>
2026-09-09 12:50:53 -0300

    ATV1: rotas simples sobre, alunos, contato

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+  Route::get('/sobre', function () {
+    return 'Esta é a página sobre.';
+});
+
+Route::get('/alunos', function () {
+    return 'Esta é a página de alunos.';
+});
+
+Route::get('/contato', function () {
+    return 'Esta é a página de contato.';
+});
+  
 });
