3661f3b28705084a7f0b27af9ce463c760e83108
KevinFaculdadeXD <engs-kevinrichardt@camporeal.edu.br>
2026-09-09 19:25:20 -0300

    ATV14: formulario de cadastro com exibicao de erros

--- resources/views/alunos/create.blade.php
+++ resources/views/alunos/create.blade.php
@@ -4,10 +4,19 @@
 
 @section('content')
     <h1>Cadastrar Aluno</h1>
+
+    @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $erro)
+                <li>{{ $erro }}</li>
+            @endforeach
+        </ul>
+    @endif
+
     <form method="POST" action="/alunos-crud">
         @csrf
-        <input type="text" name="nome" placeholder="Nome do aluno">
-        <input type="text" name="curso" placeholder="Curso do aluno">
+        <input type="text" name="nome" placeholder="Nome do aluno" value="{{ old('nome') }}">
+        <input type="text" name="curso" placeholder="Curso do aluno" value="{{ old('curso') }}">
         <button type="submit">Salvar</button>
     </form>
 @endsection
