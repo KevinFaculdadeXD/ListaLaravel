2d81d1c04484f9369a0dfa9b107a6e3dd8211bd3
KevinFaculdadeXD <engs-kevinrichardt@camporeal.edu.br>
2026-09-09 12:53:10 -0300

    ATV2: rotas com parametro produto, categoria, usuario

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,17 @@ Route::get('/alunos', function () {
 Route::get('/contato', function () {
     return 'Esta é a página de contato.';
 });
+
+  Route::get('/produto/{id}', function ($id) {
+    return "Produto número: $id";
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return "Categoria número: $id";
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return "Usuário número: $id";
+});
   
 });
