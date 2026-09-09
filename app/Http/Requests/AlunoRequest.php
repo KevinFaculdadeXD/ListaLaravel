707e68dbb5a86cb8f79a969b3aece974d708d258
KevinFaculdadeXD <engs-kevinrichardt@camporeal.edu.br>
2026-09-09 19:29:01 -0300

    DESAFIO: mensagens personalizadas de validacao

--- app/Http/Requests/AlunoRequest.php
+++ app/Http/Requests/AlunoRequest.php
@@ -18,4 +18,15 @@ class AlunoRequest extends FormRequest
             'curso' => 'required|string|max:100',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'nome.required' => 'O campo nome é obrigatório.',
+            'nome.min' => 'O nome deve ter no mínimo 3 caracteres.',
+            'nome.max' => 'O nome não pode ter mais que 100 caracteres.',
+            'curso.required' => 'O campo curso é obrigatório.',
+            'curso.max' => 'O curso não pode ter mais que 100 caracteres.',
+        ];
+    }
 }
