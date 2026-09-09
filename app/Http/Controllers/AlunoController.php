a38ebaebb95d39843fcce1356437a08b5e04a30b
KevinFaculdadeXD <engs-kevinrichardt@camporeal.edu.br>
2026-09-09 19:06:53 -0300

    ATV13: CRUD completo de alunos conectado ao banco

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -9,37 +9,54 @@ class AlunoController extends Controller
 {
     public function index()
     {
-        return 'Lista de todos os alunos';
+        $alunos = Aluno::all();
+        return view('alunos.index', compact('alunos'));
     }
 
     public function show($id)
     {
-        return "Mostrando aluno de id: $id";
+        $aluno = Aluno::findOrFail($id);
+        return view('alunos.show', compact('aluno'));
     }
 
     public function create()
     {
-        return 'Formulário de criação de aluno';
+        return view('alunos.create');
     }
 
     public function store(Request $request)
     {
-        return 'Aluno salvo com sucesso';
+        Aluno::create([
+            'nome' => $request->nome,
+            'curso' => $request->curso,
+        ]);
+
+        return redirect('/alunos-crud')->with('sucesso', 'Aluno cadastrado com sucesso!');
     }
 
     public function edit($id)
     {
-        return "Formulário de edição do aluno $id";
+        $aluno = Aluno::findOrFail($id);
+        return view('alunos.edit', compact('aluno'));
     }
 
     public function update(Request $request, $id)
     {
-        return "Aluno $id atualizado";
+        $aluno = Aluno::findOrFail($id);
+        $aluno->update([
+            'nome' => $request->nome,
+            'curso' => $request->curso,
+        ]);
+
+        return redirect('/alunos-crud')->with('sucesso', 'Aluno atualizado com sucesso!');
     }
 
     public function destroy($id)
     {
-        return "Aluno $id removido";
+        $aluno = Aluno::findOrFail($id);
+        $aluno->delete();
+
+        return redirect('/alunos-crud')->with('sucesso', 'Aluno removido com sucesso!');
     }
 
     public function porCurso($curso)
