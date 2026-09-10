00f28ca34a8e01f0ef3a722cd638e5773489f4b2
KevinFaculdadeXD <engs-kevinrichardt@camporeal.edu.br>
2026-09-09 21:00:29 -0300

    ATV18: adiciona Laravel Breeze ao projeto

--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sobre', function () {
-    return 'Esta é a página sobre.';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/alunos', function () {
-    return 'Esta é a página de alunos.';
-});
-
-Route::get('/contato', function () {
-    return 'Esta é a página de contato.';
-});
-
-Route::get('/produto/{id}', function ($id) {
-    return "Produto número: $id";
-});
-
-Route::get('/categoria/{id}', function ($id) {
-    return "Categoria número: $id";
-});
-
-Route::get('/usuario/{id}', function ($id) {
-    return "Usuário número: $id";
-});
-
-Route::get('/alunos-crud', [AlunoController::class, 'index']);
-Route::get('/alunos-crud/create', [AlunoController::class, 'create']);
-Route::get('/alunos-crud/{id}', [AlunoController::class, 'show']);
-Route::get('/alunos-crud/{id}/edit', [AlunoController::class, 'edit']);
-Route::post('/alunos-crud', [AlunoController::class, 'store']);
-Route::put('/alunos-crud/{id}', [AlunoController::class, 'update']);
-Route::delete('/alunos-crud/{id}', [AlunoController::class, 'destroy']);
-
-Route::get('/alunos/curso/{curso}', [AlunoController::class, 'porCurso']);
-Route::get('/alunos/busca/{nome}', [AlunoController::class, 'porNome']);
-Route::get('/alunos/recentes', [AlunoController::class, 'recentes']);
-Route::get('/alunos/total', [AlunoController::class, 'total']);
-
-use App\Http\Controllers\CursoController;
-
-Route::get('/cursos/{id}', [CursoController::class, 'show']);
+require __DIR__.'/auth.php';
